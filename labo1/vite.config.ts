d4caf0689bc7e06e274af5c4d0898bb2acf08599
Freud <>
2024-12-16 19:49:34 -0500

    ajout des fonctionnalite au fichier config de vite

--- labo1/vite.config.ts
+++ labo1/vite.config.ts
@@ -3,12 +3,57 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      // add this to cache all the imports
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: [
+        "**/*",
+      ],
+      manifest: {
+        "theme_color": "#ff0000",
+        "background_color": "#ff0000",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
+        "name": "Application d'affichage de Produits",
+        "short_name": "Liste de Produits",
+        "description": "Labo1 affichage liste de produits utilisant bootstrap et vueJs",
+        "icons": [
+          {
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+      }
+    }),
   ],
   resolve: {
     alias: {
