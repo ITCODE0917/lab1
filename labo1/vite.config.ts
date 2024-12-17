8d614d2b6673ddb6f2c216e04ec98fd0e3aacd55
Freud <>
2024-12-16 23:01:28 -0500

    ajout de l'icone

--- labo1/vite.config.ts
+++ labo1/vite.config.ts
@@ -33,22 +33,22 @@ export default defineConfig({
         "description": "Labo1 affichage liste de produits utilisant bootstrap et vueJs",
         "icons": [
           {
-            "src": "/icon-192x192.png",
+            "src": "/icons/icon-192x192.png",
             "sizes": "192x192",
             "type": "image/png"
           },
           {
-            "src": "/icon-256x256.png",
+            "src": "/icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "/icons/icon-256x256.png",
             "sizes": "256x256",
             "type": "image/png"
           },
           {
-            "src": "/icon-384x384.png",
-            "sizes": "384x384",
-            "type": "image/png"
-          },
-          {
-            "src": "/icon-512x512.png",
+            "src": "/icons/icon-512x512.png",
             "sizes": "512x512",
             "type": "image/png"
           }
