cb1c72ad5782e5622b6e32fe3823b3c5c28a60f5
Freud <>
2024-12-17 20:46:06 -0500

    modifciation main.ts pour ajouter scripte

--- labo1/src/main.ts
+++ labo1/src/main.ts
@@ -4,3 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './assets/style.css'
 createApp(App).mount('#app')
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/lab1/sw.js') 
+        .then((registration) => {
+          console.log('Service Worker enregistré avec succès:', registration);
+        })
+        .catch((error) => {
+          console.error('Échec de l\'enregistrement du Service Worker:', error);
+        });
+    });
+  }
+  
